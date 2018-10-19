585f4b602ba66fd46b8c3e2413e4dbf51d509754
gHjp04har556qtyw <44244977+gHjp04har556qtyw@users.noreply.github.com>
2018-10-19 18:16:20 +0100

    initial commit

---
+++ Answers/Question1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
+{
+    public class Question1
+    {
+        public static int Answer(int[] portfolios)
+        {
+            int result = 0;
+            int n = portfolios.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    int xor = portfolios[i] ^ portfolios[j];
+
+                    if (xor == 65535) return xor;
+
+                    result = xor > result ? xor : result;
+                }
+            }
+            return result;
+        }
+    }
+}
