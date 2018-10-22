c99d3e2aaefb54aeaacf120cb8a25cd2f3374272
gHjp04har556qtyw <44244977+gHjp04har556qtyw@users.noreply.github.com>
2018-10-22 01:08:12 +0100

    ....

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -12,9 +12,9 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         public static int Answer(int[] portfolios)
         {
             int result = 0;
-            for (int n = portfolios.Length; n >= 0; n--)
+            for (byte n = (byte)portfolios.Length; n >= 0; n--)
             {
-                for (int j = n + 1; j < portfolios.Length; j++)
+                for (byte j = (byte)(n + 1); j < portfolios.Length; j++)
                 {
                     result = (portfolios[n] ^ portfolios[j]) > result ? (portfolios[n] ^ portfolios[j]) : result;
                 }
