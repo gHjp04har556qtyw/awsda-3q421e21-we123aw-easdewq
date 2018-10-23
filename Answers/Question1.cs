48015e43d93134876a9aaff246d42b0e3d5f91ba
gHjp04har556qtyw <44244977+gHjp04har556qtyw@users.noreply.github.com>
2018-10-23 18:05:17 +0100

    speed

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -11,15 +11,14 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         
         public static int Answer(int[] portfolios)
         {
-            int result = 0;
-            for (int i=0, n = portfolios.Length, a = 0; i < n; i++)
+            int result = 0, i=0, n = portfolios.Length, a, j, x;
+            for (; i < n; i++)
             {
                 a = portfolios[i];
-                for (int j = i + 1, x = 0; j < n; j++)
+                for (j = i + 1, x = 0; j < n; j++)
                 {
                     x = a ^ portfolios[j];
                     result = x > result ? x : result;
-                    if (result == 65335) return 65335;
                 }
             }
             return result;
