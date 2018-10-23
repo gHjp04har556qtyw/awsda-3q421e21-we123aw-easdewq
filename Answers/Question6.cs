8eab095fb941f73932881b1f6ea92d0999ce7c40
gHjp04har556qtyw <44244977+gHjp04har556qtyw@users.noreply.github.com>
2018-10-23 19:20:34 +0100

    +1

--- Answers/Question6.cs
+++ Answers/Question6.cs
@@ -7,9 +7,14 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         public static int Answer(int numOfServers, int targetServer, int[,] connectionTimeMatrix)
         {
 
+            /*
+            int smallest = Int32.MaxValue;
 
+            // loop through until smallest edge found
+            for(int i=0; i<numOfServers; i++)
+            {
 
-
+            }*/
 
             // Eventually it will be the value in the first index to target server [0, 7, targetServer]
             return connectionTimeMatrix[0, targetServer];
