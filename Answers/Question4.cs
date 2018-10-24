5916a4ac6c1949f343426831a28a276e8ddf85bf
gHjp04har556qtyw <44244977+gHjp04har556qtyw@users.noreply.github.com>
2018-10-24 17:55:48 +0100

    broken questions??

--- Answers/Question4.cs
+++ Answers/Question4.cs
@@ -1,9 +1,32 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+﻿using System;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question4
     {
         public static int Answer(string[,] machineToBeFixed, int numOfConsecutiveMachines)
         {
+            // Brute force Loop through
+            for(int i =0; i< machineToBeFixed.GetLength(0); i++)
+            {
+                bool startCounting = false;
+
+                for(int j = 0; j < machineToBeFixed.GetLength(1); j++)
+                {
+                    // skip if X is found
+                    if (machineToBeFixed[i, j].Equals("X") && (machineToBeFixed.GetLength(1) - j) > numOfConsecutiveMachines)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        // startCounting
+                        startCounting = true;
+                    }
+                }
+            }
+
+
             //TODO: Please work out the solution;
             return 0;
         }
--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -6,27 +6,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     {
         public static int Answer(int[] numOfShares, int totalValueOfShares)
         {
-            int n = numOfShares.Length;
-
-            bool changed = false;
-            int currAlloc = 0, minAlloc = n + 1;
-
-            int start = 0, end = 0;
-            while (end < n)
-            {
-                while (currAlloc <= totalValueOfShares && end < n)
-                    currAlloc += numOfShares[end++];
-                while (currAlloc >= totalValueOfShares && start < n)
-                {
-                    if (end - start < minAlloc)
-                    {
-                        minAlloc = end - start;
-                        changed = true;
-                    }
-                    currAlloc -= numOfShares[start++];
-                }
-            }
-            return changed ? minAlloc : 0;
+            return -1;
         }
     }
 }
