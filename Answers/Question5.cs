21a066987c3ce0760c7d26a31bdd5412c40cd19f
gHjp04har556qtyw <44244977+gHjp04har556qtyw@users.noreply.github.com>
2018-10-25 14:30:51 +0100

    q5 test

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -6,7 +6,27 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     {
         public static int Answer(int[] numOfShares, int totalValueOfShares)
         {
-            return -1;
+            int n = numOfShares.Length;
+
+            bool changed = false;
+            int currAlloc = 0, minAlloc = n + 1;
+
+            int start = 0, end = 0;
+            while (end < n)
+            {
+                while (currAlloc <= totalValueOfShares && end < n)
+                    currAlloc += numOfShares[end++];
+                while (currAlloc >= totalValueOfShares && start < n)
+                {
+                    if (end - start < minAlloc)
+                    {
+                        minAlloc = end - start;
+                        changed = true;
+                    }
+                    currAlloc -= numOfShares[start++];
+                }
+            }
+            return changed ? minAlloc : 0;
         }
     }
 }
