cd3743d7844e12aa7de3e6eec2655202244ccb61
gHjp04har556qtywXouyqwejqM9a <44244977+gHjp04har556qtywXouyqwejqM9a@users.noreply.github.com>
2018-10-29 20:12:02 +0000

    q4 fixed?

--- Answers/Question4.cs
+++ Answers/Question4.cs
@@ -1,41 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question4
     {
+            
+
+
         public static int Answer(string[,] machineToBeFixed, int numOfConsecutiveMachines)
         {
+            List<Int32> list = new List<Int32>();
+
             int min = Int32.MaxValue;
             bool canFix = false;
-            int systemSize = machineToBeFixed.GetLength(0);
-            int pcSize = machineToBeFixed.GetLength(1);
-
-            // don't count if we can't
-            if (pcSize < numOfConsecutiveMachines) return 0;
-
-            int diff = Math.Abs(pcSize - numOfConsecutiveMachines);
-
-            int count = 0;
-            int sum = 0;
-
-            for (int i = 0; i < systemSize; i++)
+            int sum;
+            int count;
+            int val;
+            for (int i = 0; i < machineToBeFixed.GetLength(0); i++)
             {
-                for (int j = 0; j < pcSize; j++)
+                list.Clear();
+                sum = 0;
+                count = 0;
+                for (int j = 0; j < machineToBeFixed.GetLength(1); j++)
                 {
-                    var pc = machineToBeFixed[i, j];
-                    if (pc.Length == 1) {
-                        if (pc.Equals("X"))
+                    string pc = machineToBeFixed[i, j];
+
+                    if (pc.Length == 1 && pc.Equals("X"))
+                    {
+                        // so we don't clear twice incase the list starts with X
+                        if (count != 0)
                         {
+                            list.Clear();
                             count = 0;
                             sum = 0;
                         }
                     }
                     else
                     {
+                        val = Convert.ToInt32(pc);
+                        sum += val;
+                        count++;
+                        list.Add(val);
+                        if (count == numOfConsecutiveMachines)
+                        {
+                            canFix = true;
+                            if (sum < min)
+                            {
+                                min = sum;
+                            }
+                            sum -= list[0];
+                            list.RemoveAt(0);
+                        }
+                    }
+                }
+            }
+            return canFix ? min : 0;
+        }
+
+        /*
+         * int min = Int32.MaxValue;
+            bool canFix = false;
+            int systemSize = machineToBeFixed.GetLength(0);
+            int pcSize = machineToBeFixed.GetLength(1);
+
+            // don't count if we can't
+            if (pcSize < numOfConsecutiveMachines)
+            {
+                return 0;
+            }
+
+            int count = 0;
+            int sum = 0;
+
+            for (int i = 0; i < systemSize; i++)
+            {
+                count = 0;
+                sum = 0;
+                for (int j = 0; j < pcSize; j++)
+                {
+                    var pc = machineToBeFixed[i, j];
+                    if (pc.Length == 1 && pc.Equals("X"))
+                    {
+                        count = 0;
+                        sum = 0;
+                    }
+                    else
+                    {
                         count++;
                         sum += Convert.ToInt32(pc);
-
 
                         if (count == numOfConsecutiveMachines)
                         {
@@ -50,8 +103,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 }
             }
 
-            return canFix ? min : 0;
-        }
+            return canFix ? min : 0;*/
 
         /*   
         public static bool canFix(string[] system, int numOfConsecutiveMachines)
