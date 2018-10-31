3dfd20cc575ac21b615a303048cbd4252f9fedf6
gHjp04har556qtywXouyqwejqM9a <44244977+gHjp04har556qtywXouyqwejqM9a@users.noreply.github.com>
2018-10-31 00:00:12 +0000

    test

--- Answers/Question2.cs
+++ Answers/Question2.cs
@@ -1,32 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
     {
+        public static int Answer(int[] cashflowIn, int[] cashflowOut)
+        {
+            int sum1 = getSum(cashflowIn);
+            int sum2 = getSum(cashflowOut);
+            int len1 = cashflowIn.Length;
+            int len2 = cashflowOut.Length;
+            // subset sums
+            List<int> inSums = isSubsetSum(cashflowIn, cashflowIn.Length, sum1);
+            List<int> outSums = isSubsetSum(cashflowOut, cashflowOut.Length, sum2);
+            Dictionary<int, int> _dictionary = new Dictionary<int, int>();
+            foreach (var x in inSums)
+            {
+                _dictionary.Add(x, 1);
+            }
 
-        // Helper method to return the sum of an array
-        public static int SumArray(int[] arr, int n)
+            foreach (var z in outSums)
+            {
+                if (!_dictionary.ContainsKey(z))
+                {
+                    _dictionary.Add(z, 1);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            // add empty set
+            _dictionary.Add(0, 1);
+            // convert the dictionary to an array
+            int[] arr = (new List<int>(_dictionary.Keys)).ToArray();
+            // return the lowest difference in the array
+            return findMinDiff(arr, arr.Length);
+        }
+
+        static int findMinDiff(int[] arr, int n)
+        {
+            Array.Sort(arr);
+            int diff = int.MaxValue;
+            for (int i = 0; i < n - 1; i++)
+                if (arr[i + 1] - arr[i] < diff)
+                    diff = arr[i + 1] - arr[i];
+            return diff;
+        }
+
+        static int getSum(int[] nums)
         {
             int sum = 0;
-            for(int i=0; i<n; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
-                sum += arr[i];
+                sum += nums[i];
             }
+
             return sum;
         }
 
-
-        public static int Answer(int[] cashflowIn, int[] cashflowOut)
+        static List<int> isSubsetSum(int[] set, int n, int sum)
         {
-            int sumA = SumArray(cashflowIn, cashflowIn.Length);
-            int sumB = SumArray(cashflowOut, cashflowOut.Length);
+            List<int> nums = new List<int>();
+            // The value of subset[i][j] will be true if there  
+            // is a subset of set[0..j-1] with sum equal to i 
+            bool[,] subset = new bool[sum + 1, n + 1];
+            // If sum is 0, then answer is true 
+            for (int i = 0; i <= n; i++)
+                subset[0, i] = true;
+            // If sum is not 0 and set is empty, then answer is false 
+            for (int i = 1; i <= sum; i++)
+                subset[i, 0] = false;
+            // Fill the subset table in botton up manner 
+            for (int i = 1; i <= sum; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    subset[i, j] = subset[i, j - 1];
+                    if (i >= set[j - 1])
+                        subset[i, j] = subset[i, j] || subset[i - set[j - 1], j - 1];
+                }
+            }
 
+            for (int i = 1; i <= sum; i++)
+            {
+                if (subset[i, n])
+                {
+                    nums.Add(i);
+                }
+            }
 
-            // Dynamically add smallest positive number that also has a small negative number
-            // If there is no small negative, don't use it, try other solutions until 0 is found
-
-            return 0;
+            return nums;
         }
     }
 }
